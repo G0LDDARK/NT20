1485a92d3d3ebf3bb03edef1a365cf67a6b6471b
_GLDDRK_ <g0lddark.linux@gmail.com>
2022-09-15 13:05:14 +0200

    1.2
    Version 1.2

--- main.py
+++ main.py
@@ -1,47 +1,25 @@
-# Made by GLDDRK
-# Importationd des modules
+#Made by GLDDRK: glddrk.fr
+#Importation des modules
 import os
-
-# Fonction qui initalise le script
+#Fonction principale et de calcul
 def main():
-    os.system("clear||cls")
-    print("Ce programme a été créer par GLDDRK, version 1.1")
-    print("Bienvenue sur les calculateur de note sur 20 et de pourcentage")
-    PN = input("Quel fonctionnalité du programme voulez vous utiliser? P pour Poucentage, N pour Note")
-    if PN.lower() == str("n"):
-        note()
-    elif PN.lower() == str("p"):
-        pourcentage()
-    else:
-        print(PN, "n'est pas un choix possible!")
-        main()
-
-# Fonction qui calcule la note
-def note():
-    note = input("Quelle est votre note : "); fnote = float(note)
-    sur = input("Sur combien est votre note : "); fsur = float(sur)
-    notesur = fnote / fsur * 20; snotesur = str(notesur)
-    print("Votre note de", note, "sur", sur, "correspond a", snotesur, "/ 20")
+    os.system("cls||clear")
+    print("Ce programme a été créer par GLDDRK, version 1.2")
+    print("Programme de calcul de note en note sur 20")
+    nt = input("Quelle est votre note : "); fnt = float(nt)
+    sr = input("Sur combien est votre note : "); fsr = float(sr)
+    ntsr = fnt / fsr * 20; sntsr = str(ntsr)
+    print("Votre note de", note, "sur", sur, "correspond à", snotesur, "/ 20")
     restart()
 
-# Fonction qui calcule le pourcentage
-def pourcentage():
-    cb = input("Combien : "); fcb = float(cb)
-    sur = input("Sur combien : "); fsur = float(sur)
-    pr = fcb / fsur * 100; spr = float(pr)
-    print(cb, "sur", sur, "représente", pr, "%")
-    restart
-
-# Fonction qui permet de recommencer
+#Fonction qui permet de recommencer
 def restart():
-    r = input("Voulez vous recommencer O : oui / Autre : non ")
-    if r.lower == str("o"):
+    a = input("Voulez vous recommencer O : oui / Autre : non ")
+    if a.lower == str("o"):
         os.system("clear||cls")
         main()
     else:
         os.system("clear||cls")
         print("Mecri de m'avoir utiliser")
         exit()
-
 main()
-#Калашников
